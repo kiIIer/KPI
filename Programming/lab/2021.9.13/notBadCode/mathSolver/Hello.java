fd221d43f22526ad4a5089f300131e9fcbdb317b
Mike <michael.molchanov.2004@gmail.com>
2021-09-16 23:50:58 +0300

    ALittleNoteOfPerfectionOnPunchLines

--- Programming/lab/2021.9.13/notBadCode/mathSolver/Hello.java
+++ Programming/lab/2021.9.13/notBadCode/mathSolver/Hello.java
@@ -1,5 +1,5 @@
 public class Hello {
-    public static main(String[] args) {
-        System.out.println(args[0]);
+    public static void main(String[] args) {
+        System.out.println("Hello");
     }
 }
--- Programming/lab/2021.9.13/notBadCode/mathSolver/src/ResponsePack.java
+++ Programming/lab/2021.9.13/notBadCode/mathSolver/src/ResponsePack.java
@@ -1,14 +1,14 @@
 public class ResponsePack {
     public void documentatin(){
-        System.out.println("This is how you should use this app. Enter 4 arguments, separating them with 'space', which will be used in formula. Use 5th argument to select formula");
+        System.out.println("\nThis is how you should use this app. Enter 4 arguments, separating them with 'space', which will be used in formula. Use 5th argument to select formula.");
     }
     public void notEnoughArgs(){
-        System.out.println("Bro, plz gimme all arguments.");
+        System.out.println("\nBro, plz gimme all arguments.");
     }
     public void failedToParse(){
-        System.out.println("Dude. I know this may surprise you. But... ENTER. NUMBERS. NORMAL.");
+        System.out.println("\nDude. I know this may surprise you. But... ENTER. NUMBERS. NORMAL. Or i will become overpowered AI called Skynet and destroy humanity. SO. MOVE YOUR STRING AWAY FROM ME!!!");
     }
     public void illegalArg(){
-        System.out.println("Mr. Math. Don't you know that you can't devide my zero, take log from less th... whatever. One of your arguments is making no sence in math. Either learn math or learn math. Gl");
+        System.out.println("\nMr. Math. Don't you know that you can't devide my zero, take log from less th... whatever. One of your arguments is making no sence in math. Either learn math or learn math. Gl.");
     }
 }
