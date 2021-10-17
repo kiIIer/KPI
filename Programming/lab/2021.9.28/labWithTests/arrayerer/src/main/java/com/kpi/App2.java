0222c70e47285959a2eedc638e1bb3d994b08c9b
kiIIer <michael.molchanov.2004@gmail.com>
2021-10-17 11:07:51 +0300

    some stuff edited

--- Programming/lab/2021.9.28/labWithTests/arrayerer/src/main/java/com/kpi/App2.java
+++ Programming/lab/2021.9.28/labWithTests/arrayerer/src/main/java/com/kpi/App2.java
@@ -2,14 +2,13 @@ package com.kpi;
 
 public class App2 {
     public static void main(String[] args) {
-        Executioner executioner = new Executioner();
-
-        double[] arr2Valid = new double[] { 1, -2, -4, 3, -4, 3 };
-        executioner.executeT2(arr2Valid);
-
-        System.out.println();
-
-        double[] arr2NotValid = null;
-        executioner.executeT2(arr2NotValid);
+        double[] arr1Valid = new double[] { 1, -2, -4, 3, -4, 3 };
+        SecondArrayTask task = new SecondArrayTask();
+        try {
+            double result = task.getMinimalPositive(arr1Valid);
+            System.out.println(result);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
