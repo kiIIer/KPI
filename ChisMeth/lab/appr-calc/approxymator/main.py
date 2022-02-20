3a621024a9aac533981502550cd2dbf71ff50d1a
killer <michael.molchanov.2004@gmail.com>
2022-02-20 20:01:47 +0200

    3 methods

---
+++ ChisMeth/lab/appr-calc/approxymator/main.py
@@ -0,0 +1,97 @@
+import math
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def f(x):
+    return np.tan(x) - 5 * np.power(x, 2) + 1
+
+
+def df(x):
+    return (1 / np.cos(x)) - 10 * x
+
+
+def halfDivRoot(start, interval, stop, which):
+    n = start
+    nn = start + interval
+    while True:
+        if f(n) * f(nn) < 0:
+            if which == 1:
+                break
+            else:
+                which -= 1
+        n += interval
+        nn += interval
+
+    fn = f(n)
+    fnn = f(nn)
+    while True:
+        if fn * fnn < 0:
+            xaver = (n + nn) / 2
+            faver = f(xaver)
+            if faver < stop:
+                return xaver
+            if fn * faver > 0:
+                n = xaver
+                fn = faver
+            else:
+                nn = xaver
+                fnn = faver
+
+
+def hordeRoot(start, interval, stop, which):
+    n = start
+    nn = start + interval
+    while True:
+        if f(n) * f(nn) < 0:
+            if which == 1:
+                break
+            else:
+                which -= 1
+        n += interval
+        nn += interval
+
+    fn = f(n)
+    fnn = f(nn)
+
+    while True:
+        if fn * fnn < 0:
+            xhor = n - fn * ((nn - n) / (fnn - fn))
+            fhor = f(xhor)
+            if fhor < stop:
+                return xhor
+            if fn * fhor > 0:
+                n = xhor
+                fn = fhor
+            else:
+                nn = xhor
+                fnn = fhor
+
+
+def newtRoot(start, stop):
+    n = start
+    while True:
+        fn = f(n)
+        if abs(fn) < stop:
+            return n
+        dfn = df(n)
+        if dfn == 0:
+            return None
+        n = n - fn / dfn
+
+
+x = np.arange(-1, 1, 0.1)
+y = f(x)
+
+plt.xlabel("x")
+plt.ylabel("f(x)")
+
+plt.axhline(0, color='black')
+plt.axvline(0, color='black')
+
+plt.plot(x, y, "r")
+plt.show()
+
+print(halfDivRoot(-1, 0.1, 0.000001, 1))
+print(hordeRoot(-1, 0.1, 0.000001, 1))
+print(newtRoot(-1, 0.000001))
