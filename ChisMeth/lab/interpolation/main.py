9b91748e1a5ac6cef7301eb195157ddb095f6d56
killer <michael.molchanov.2004@gmail.com>
2022-04-10 22:39:16 +0200

    L method

--- ChisMeth/lab/interpolation/main.py
+++ ChisMeth/lab/interpolation/main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
-
-# Press ⌃F5 to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press F9 to toggle the breakpoint.
+def phiOfX(x, data):
+    result = 1
+    i = 0
+    while i < len(data):
+        result = result * (x - data[i])
+        i += 1
+    return result
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def phiTickOfX(i, data):
+    result = 1
+    j = 0
+    while j < len(data):
+        if i != j:
+            result = result * (data[i] - data[j])
+        j += 1
+    return result
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def interpolatedFLag(x, datax, datay):
+    result = 0
+    i = 0
+    while i < len(datax):
+        result = result + (datay[i] * phiOfX(x, datax)) / (phiTickOfX(i, datax) * (x - datax[i]))
+        i += 1
+    return result
+
+
+def f(x):
+    return np.cos(x) / np.sin(x)
+
+
+x = np.linspace(np.pi/8, np.pi/2, 100)
+xd = [np.pi / 8, np.pi / 4, 3 * np.pi / 8, np.pi / 2]
+yd = [np.sqrt(2) + 1, 1, np.sqrt(2) - 1, 0]
+plt.plot(x, interpolatedFLag(x, xd, yd))
+plt.plot(x, f(x), 'r')
+plt.show()
