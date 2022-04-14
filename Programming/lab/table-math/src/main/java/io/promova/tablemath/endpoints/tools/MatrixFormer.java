8f5530da7f3dae47121091140b97667c216069d8
killer <michael.molchanov.2004@gmail.com>
2022-04-14 22:43:10 +0200

    chenged pool, there is indexoutofbounds

--- Programming/lab/table-math/src/main/java/io/promova/tablemath/endpoints/tools/MatrixFormer.java
+++ Programming/lab/table-math/src/main/java/io/promova/tablemath/endpoints/tools/MatrixFormer.java
@@ -7,6 +7,7 @@ import java.time.Year;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
 
 public class MatrixFormer implements IMatrixFormer
@@ -21,8 +22,9 @@ public class MatrixFormer implements IMatrixFormer
     @Override
     public void form(Matrix matrix, Parameter paramA, Parameter paramB)
     {
+        ExecutorService ex = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
         int index = 0;
-        ExecutorService ex = Executors.newCachedThreadPool();
         for (long i = paramA.getLowerBound(); i < paramA.getUpperBound(); i += paramA.getStep(), index++)
         {
             RowFormer rowFormer = new RowFormer(i, paramB, matrix.getRow(index), formulaSolver);
@@ -32,7 +34,7 @@ public class MatrixFormer implements IMatrixFormer
 
         try
         {
-            ex.awaitTermination(1, TimeUnit.DAYS);
+            ex.awaitTermination(0, TimeUnit.DAYS);
         } catch (InterruptedException e)
         {
             e.printStackTrace();
