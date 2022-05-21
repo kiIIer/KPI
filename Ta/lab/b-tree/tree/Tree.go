151e19a403390c4a61b788d40e26bee5c0f6b9c9
killer <michael.molchanov.2004@gmail.com>
2022-05-21 20:36:33 +0200

    this fuckery is done

--- Ta/lab/b-tree/tree/Tree.go
+++ Ta/lab/b-tree/tree/Tree.go
@@ -11,40 +11,69 @@ type Tree[K constraints.Ordered, V any] struct {
 }
 
 func NewTree[K constraints.Ordered, V any](t int) *Tree[K, V] {
-	node := NewNode[K, V](t)
 
 	return &Tree[K, V]{
 		t:    t,
-		root: node,
+		root: nil,
 	}
+}
+
+func (tree *Tree[K, V]) Delete(key K) {
+	if tree.root == nil {
+		return
+	}
+	tree.root.delete(key)
+
+	if tree.root.n == 0 {
+		if tree.root.isLeaf {
+			tree.root = nil
+		} else {
+			tree.root = tree.root.nodes[0]
+		}
+
+	}
+}
+
+func (tree *Tree[K, V]) Insert(key K, value V) {
+	toInsert := NewElement(key, value)
+
+	if tree.root == nil {
+		tree.root = NewNode[K, V](tree.t, true)
+		tree.root.elements[0] = toInsert
+		tree.root.n = 1
+	} else {
+		if tree.root.n == 2*tree.t-1 {
+			newRoot := NewNode[K, V](tree.t, false)
+
+			newRoot.nodes[0] = tree.root
+
+			newRoot.split(0, tree.root)
+
+			i := 0
+			if newRoot.elements[0].key < toInsert.key {
+				i++
+			}
+			newRoot.insertUtil(toInsert)
+
+			tree.root = newRoot
+		} else {
+			tree.root.insertUtil(toInsert)
+		}
+	}
+}
+
+func (tree *Tree[K, V]) Search(key K) *Element[K, V] {
+	if tree.root == nil {
+		return nil
+	}
+	return tree.root.search(key)
 }
 
 func (tree *Tree[K, V]) Slice() []*Element[K, V] {
-	var slice []*Element[K, V]
-	if len(tree.root.elements) == 0 {
-		return slice
+	if tree.root == nil {
+		return []*Element[K, V]{}
 	}
-
-	slice = append(slice, tree.sliceUtil(tree.root)...)
-
-	return slice
-}
-
-func (tree *Tree[K, V]) sliceUtil(node *Node[K, V]) []*Element[K, V] {
-	var slice []*Element[K, V]
-	for i := 0; i < tree.t-1; i++ {
-		currentNode := node.nodes[i]
-		if currentNode != nil {
-			slice = append(slice, tree.sliceUtil(currentNode)...)
-		}
-
-		currentElement := node.elements[i]
-		if currentElement != nil {
-			slice = append(slice, currentElement)
-		}
-	}
-
-	return slice
+	return tree.root.slice()
 }
 
 func (tree *Tree[K, V]) String() string {
